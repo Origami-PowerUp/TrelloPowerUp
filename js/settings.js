0daa7c2a82e514608792392d9dda4dff1a1a8907
MijazApps <mijazapps@gmail.com>
2017-12-07 14:06:10 +0500

    Update settings.js
    test

--- js/settings.js
+++ js/settings.js
@@ -211,7 +211,7 @@ $("#startTimer").on('click', function(e){
                                         console.log('recording  = ', recording)
                                         t.get('card','shared','assignedTo').then(function(assignedTo){
                                             console.log('assgiendTo in else .,, = ', assignedTo)
-                                        if(assignedTo && assignedTo.includes("Muhamad Shehzad")){
+                                        if(assignedTo && assignedTo.includes("Muhamad3435244Shehzad")){
                                             console.log('user matched......')
                                                 t.set('board', 'private', 'recording', {status: false, cardUrl: ''}).then(function () {
                                                     //  console.log('timer cleared', assignedTo)
