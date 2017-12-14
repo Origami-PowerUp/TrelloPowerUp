47ebafe2a2e8fb97cc9ba81ede4e4eb4bbb02b71
MijazApps <mijazapps@gmail.com>
2017-12-14 17:54:45 +0500

    Update client.js
    test

--- js/client.js
+++ js/client.js
@@ -583,8 +583,16 @@ TrelloPowerUp.initialize({
 //      // usually you will provide a callback function to be run on button click
 //      // we recommend that you use a popup on click generally
      // icon: GRAY_ICON, // don't use a colored icon here
-      text: 'Open Popup',
+      text: 'Origami-PowerUP',
       callback: function(t){
+        return t.board('members').then(function(boardMembers){
+                return t.popup({
+                  title: 'On-Time Project Management',
+                  url: 'https://origami-powerup.github.io/TrelloPowerUp/settings.html',
+                  args: {members:boardMembers},
+                  height: 184 // we can always resize later, but if we know the size in advance, its good to tell Trello
+                });        
+              })
       }
 //    }, {
 //      // but of course, you could also just kick off to a url if that's your thing
@@ -647,7 +655,7 @@ TrelloPowerUp.initialize({
     // it is the least disruptive, and fits in well with the rest of Trello's UX
     return t.popup({
       title: 'Settings',
-      url: './powerup.html',
+      url: 'https://origami-powerup.github.io/TrelloPowerUp/powerup.html',
       height: 184 // we can always resize later, but if we know the size in advance, its good to tell Trello
     });
   }
